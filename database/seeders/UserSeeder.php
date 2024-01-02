2e5e58621670e65236b4699b71df66523761a0cb
Braiani <felipe.braianitech@gmail.com>
2024-01-02 00:25:08 -0400

    Added Admin user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         \App\Models\User::factory()->create([
+             'name' => 'Administrator',
+             'email' => 'admin@admin.com',
+         ]);
+    }
+}
